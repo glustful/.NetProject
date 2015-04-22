d10679dfc7ae6bf49b6c388ab6993a880594c06d
yangdingpeng <evesgf@163.com>
2015-04-22 10:48:56 +0800

    CRMcontroller添加

---
+++ Dev/Code/Zerg/Controllers/CRMBrokerController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Zerg.Controllers
+{
+    /// <summary>
+    /// CRM 经纪人
+    /// </summary>
+    public class CRMBrokerController : ApiController
+    {
+    }
+}
