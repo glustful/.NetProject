13a6eda7f68a6ad2c682d609492d3be76420df0a
李洪亮 <llhhll004@163.com>
2015-04-28 16:37:32 +0800

    fuck   什么都没有改

--- Dev/Code/Zerg/Controllers/CRM/CRMAdminController.cs
+++ Dev/Code/Zerg/Controllers/CRM/CRMAdminController.cs
@@ -26,23 +26,7 @@ namespace Zerg.Controllers
 
         #region 任务配置 杨定鹏 2015年4月28日10:04:08
 
-        /// <summary>
-        /// 返回任务列表z
-        /// </summary>
-        /// <param name="id">分页ID</param>
-        /// <param name="typeid">任务类型</param>
-        /// <returns></returns>
-        public List<TaskModel> TaskList(int typeid)
-        {
-            //var list=new TaskSearchCondition();
-            //var type=_taskTypeService.GetTaskTypeById(typeid);
-            //var list1=_taskService.GetTasksByCondition(list).ToList().Where(p => p.TaskType == type);
-
-            //TaskModel temp = (from t in list1 select t).ToList();
-
-            return null;
-        }
-
+      
         #endregion
     }
 }
