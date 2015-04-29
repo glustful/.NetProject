d5ba4b5a52ee523e37d4c3e28663ddf88dee8db2
yangdingpeng <evesgf@163.com>
2015-04-29 16:27:35 +0800

    提交更改

--- Dev/Code/Zerg/Controllers/CRM/CRMAdminController.cs
+++ Dev/Code/Zerg/Controllers/CRM/CRMAdminController.cs
@@ -3,6 +3,9 @@ using System.Linq;
 using System.Web.Http;
 using CRM.Entity.Model;
 using CRM.Service.Task;
+using CRM.Service.TaskAward;
+using CRM.Service.TaskPunishment;
+using CRM.Service.TaskTag;
 using CRM.Service.TaskType;
 using Zerg.Models.CRM;
 using System.Net.Http;
@@ -22,16 +25,26 @@ namespace Zerg.Controllers
         private readonly ITaskService _taskService;
         private readonly ITaskTypeService _taskTypeService;
         private readonly ILevelService _levelService;
+        private readonly ITaskAwardService _taskAwardService;
+        private readonly ITaskTagService _taskTagService;
+        private readonly ITaskPunishmentService _taskPunishmentService;
 
-        public CRMAdminController(ITaskService taskService, ITaskTypeService taskTypeService,ILevelService levelService)
+        public CRMAdminController(ITaskService taskService, 
+            ITaskTypeService taskTypeService,
+            ILevelService levelService,
+            ITaskAwardService taskAwardService,
+            ITaskTagService taskTagService,
+            ITaskPunishmentService taskPunishmentService)
         {
             _taskService = taskService;
             _taskTypeService = taskTypeService;
             _levelService=levelService;
+            _taskAwardService = taskAwardService;
+            _taskTagService = taskTagService;
+            _taskPunishmentService = taskPunishmentService;
         }
 
         #region 任务配置 杨定鹏 2015年4月28日10:04:08
-
         /// <summary>
         /// 返回任务列表
         /// </summary>
@@ -65,51 +78,174 @@ namespace Zerg.Controllers
         #region 任务类型 杨定鹏 2015年4月28日17:13:12
 
         /// <summary>
-        /// 添加任务类型表
+        /// 添加和修改任务类型表
         /// </summary>
-        /// <param name="name"></param>
-        /// <param name="describe"></param>
+        /// <param name="taskTypeModel">任务类型数据模型</param>
+        /// <param name="type">操作状态，新增/修改</param>
         /// <returns></returns>
-        //public AccsessModel AddTaskTpye(string name, string describe)
-        //{
-        //    if (!string.IsNullOrWhiteSpace(name))
-        //    {
-        //        var model = new TaskTypeEntity()
-        //        {
-        //            Name = name,
-        //            Describe = describe
-        //        };
-        //        _taskTypeService.Create(model);
-        //    }
-        //    else
-        //    {
-        //        return new AccsessModel {status = "添加失败", msg = "类型名称不能为空"};;
-        //    }
-
-        //    return new AccsessModel { status = "添加成功", msg = "ok" }; ;
-        //}
-
-        ///// <summary>
-        ///// 删除任务类型
-        ///// </summary>
-        ///// <param name="id"></param>
-        ///// <returns></returns>
-        //public AccsessModel DelTaskType(int id)
-        //{
-        //    _taskTypeService.Delete(_taskTypeService.GetTaskTypeById(id));
-        //    return new AccsessModel { status = "添加成功", msg = "ok" }; ;
-        //}
-
-        public List<TaskTypeModel> TaskTypeList()
+        public ResultModel AddTaskTpye(TaskTypeModel taskTypeModel,string type)
         {
-            var condition = new TaskTypeSearchCondition();
-            var list = _taskTypeService.GetTaskTypesByCondition(condition).Select(a => new TaskTypeModel(
-                )).ToList();
+            if (!string.IsNullOrWhiteSpace(taskTypeModel.Name))
+            {
+                var model = new TaskTypeEntity()
+                {
+                    Name = taskTypeModel.Name,
+                    Describe = taskTypeModel.Describe
+                };
+                if (type == "add")
+                {
+                    _taskTypeService.Create(model);
+                }
+                if (type == "edit")
+                {
+                    _taskTypeService.Update(_taskTypeService.GetTaskTypeById(taskTypeModel.Id));
+                }
+            }
+            else
+            {
+                return new ResultModel { Status = false, Msg = "类型名称不能为空" }; ;
+            }
+            return new ResultModel { Status = true, Msg = "操作成功" }; ;
+        }
 
-            return null;
+        /// <summary>
+        /// 删除任务类型
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public ResultModel DelTaskType(int id)
+        {
+            _taskTypeService.Delete(_taskTypeService.GetTaskTypeById(id));
+            return new ResultModel { Status = true, Msg = "删除成功" }; ;
         }
 
         #endregion
+
+        #region 任务奖励 杨定鹏 2015年4月29日15:15:41
+        /// <summary>
+        /// 添加和修改任务奖励
+        /// </summary>
+        /// <param name="taskAwardModel">任务奖励数据模型</param>
+        /// <param name="type">操作状态，新增/修改</param>
+        /// <returns></returns>
+        public ResultModel AddTaskAward(TaskAwardModel taskAwardModel, string type)
+        {
+            if (!string.IsNullOrWhiteSpace(taskAwardModel.Name))
+            {
+                var model = new TaskAwardEntity()
+                {
+                    Name = taskAwardModel.Name,
+                    Describe = taskAwardModel.Describe,
+                    Value=taskAwardModel.Value
+                };
+                if (type == "add")
+                {
+                    _taskAwardService.Create(model);
+                }
+                if (type == "edit")
+                {
+                    _taskAwardService.Update(_taskAwardService.GetTaskAwardById(taskAwardModel.Id));
+                }
+            }
+            else
+            {
+                return new ResultModel { Status = false, Msg = "类型名称不能为空" }; ;
+            }
+            return new ResultModel { Status = true, Msg = "操作成功" }; ;
+        }
+
+        /// <summary>
+        /// 删除任务奖励
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public ResultModel DelTaskAward(int id)
+        {
+            _taskAwardService.Delete(_taskAwardService.GetTaskAwardById(id));
+            return new ResultModel{Status = true,Msg = "删除成功"};
+        }
+
+        #endregion
+
+        #region 任务目标 杨定鹏 2015年4月29日15:57:37
+        /// <summary>
+        /// 添加和修改任务目标
+        /// </summary>
+        /// <param name="taskTagModel">任务目标数据模型</param>
+        /// <param name="type">操作状态 新增/修改</param>
+        /// <returns></returns>
+        public ResultModel AddTaskTag(TaskTagModel taskTagModel,string type)
+        {
+            if (!string.IsNullOrWhiteSpace(taskTagModel.Name))
+            {
+                var model = new TaskTagEntity()
+                {
+                    Name = taskTagModel.Name,
+                    Describe = taskTagModel.Describe,
+                    Value = taskTagModel.Value
+                };
+                if (type == "add")
+                {
+                    _taskTagService.Create(model);
+                }
+                if (type == "edit")
+                {
+                    _taskTagService.Update(_taskTagService.GetTaskTagById(taskTagModel.Id));
+                }
+            }
+            else
+            {
+                return new ResultModel { Status = false, Msg = "类型名称不能为空" };
+            }
+            return new ResultModel { Status = true, Msg = "操作成功" };
+        }
+
+        /// <summary>
+        /// 删除任务奖励
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public ResultModel DelTaskTag(int id)
+        {
+            _taskTagService.Delete(_taskTagService.GetTaskTagById(id));
+            return new ResultModel { Status = true, Msg = "删除成功" };
+        }
+        #endregion
+
+        #region 任务惩罚 杨定鹏 2015年4月29日16:21:10
+        /// <summary>
+        /// 添加和修改任务惩罚
+        /// </summary>
+        /// <param name="taskPunishmentModel"></param>
+        /// <param name="type"></param>
+        /// <returns></returns>
+        //public ResultModel AddTaskPunishment(TaskPunishmentModel taskPunishmentModel, string type)
+        //{
+        //    if (!string.IsNullOrWhiteSpace(taskPunishmentModel.Name))
+        //    {
+        //        var model = new TaskPunishmentEntity()
+        //        {
+        //            Name = taskPunishmentModel.Name,
+        //            Describe = taskPunishmentModel.Describe,
+        //            Value = taskPunishmentModel.Value
+        //        };
+        //        if (type == "add")
+        //        {
+        //            _.Create(model);
+        //        }
+        //        if (type == "edit")
+        //        {
+        //            _taskTagService.Update(_taskTagService.GetTaskTagById(taskTagModel.Id));
+        //        }
+        //    }
+        //    else
+        //    {
+        //        return new ResultModel { Status = false, Msg = "类型名称不能为空" };
+        //    }
+        //    return new ResultModel { Status = true, Msg = "操作成功" };
+        //}
+        #endregion
+
         #endregion
 
         #region 等级  李洪亮 2015 4 28
@@ -150,8 +286,9 @@ namespace Zerg.Controllers
         {
             return PageHelper.toJson(" ");
         }
-        
+
         #endregion
+
 
 
     }
