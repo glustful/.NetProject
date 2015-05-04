aadd383ce986808ca3d8aef5d613fb9cc8a4dfd2
黄秀宇 <550206659@qq.com>
2015-05-04 19:37:53 +0800

    驼绒蒿

---
+++ Dev/Code/Zerg/Controllers/CRM/BrokerInfoController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Zerg.Controllers.CRM
+{
+    /// <summary>
+    /// 经纪人管理  李洪亮  2015-05-04
+    /// </summary>
+    public class BrokerInfoController : ApiController
+    {
+         
+
+        public BrokerInfoController()
+        {
+
+        }
+
+
+
+    }
+}
