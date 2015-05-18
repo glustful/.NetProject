6e8b0d5b5afe71a29efed031f5d77fe41a46f0a0
黄秀宇 <550206659@qq.com>
2015-05-18 21:51:58 +0800

    的

---
+++ Dev/Page/PC/app/scripts/filters/toDate.js
@@ -0,0 +1,17 @@
+/**
+ * Created by 黄秀宇 on 2015/5/13.
+ * 自定义json日期格式转换过滤器toDate
+ */
+angular.module('app').filter('toDate',function(){
+    return function(jsonDate){
+        //获取毫秒数
+        var oldtime= jsonDate.replace(/[^0-9]/ig,"");
+        //转成日期
+        var newtime = new Date();
+        newtime.setTime (oldtime);
+        //拼凑日期
+        var nntime=newtime.getFullYear()+'-'+(newtime.getMonth()+ 1)+'-'+newtime.getDate()
+            +' '+newtime.getHours ()+':'+newtime.getMinutes ();
+        return nntime;
+    }
+});
