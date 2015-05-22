58b8e3d0b1bcc3fb9c45b48dedfb1992cd0cf700
杨波 <yb@yunjoy.cn>
2015-05-22 17:39:46 +0800

    修复错误的冲突解决

--- Dev/Page/Mobile/Common/scripts/config.router.js
+++ Dev/Page/Mobile/Common/scripts/config.router.js
@@ -11,35 +11,6 @@
 /**
  * 由于不适用项目老方法是用的路由策略被抛弃
  */
-//app.config(
-//    ['$routeProvider', function ($routeProvider) {
-//        $routeProvider
-//          .when('/Index', {
-//              templateUrl: 'modules/Index/view/index.html',
-//              data: { title: '首页' },
-//              controller: 'modules/Index/render/index.js'
-//              //resolve: load(['modules/Index/render/index.js'])
-//              //              access:["admin"]
-//          })
-//
-//          .when('/Activity', {
-//              templateUrl: 'modules/activity/view/activity.html',
-//              data: { title: 'activity' }
-//              //              resolve: load(['scripts/controllers/chart.js','scripts/controllers/vectormap.js']),
-//              //              access:["admin"]
-//          })
-//
-//          .otherwise({
-//              redirectTo: 'app/Index'
-//          })
-//
-//        //-----------------------end-------------------
-//
-//    }
-//    ]
-//  );
-
-
 app.config(['$stateProvider', '$urlRouterProvider',function($stateProvider, $urlRouterProvider){
     $urlRouterProvider
         .otherwise('/app/home');
@@ -72,60 +43,6 @@ app.config(['$stateProvider', '$urlRouterProvider',function($stateProvider, $url
             url:'/hero',
             templateUrl:'modules/hero/view/hero.html'
         })
-}]);'use strict';
-
-/**
- * @ngdoc function
- * @name app.config:uiRouter
- * @description
- * # Config
- * Config for the router
- */
-
-/**
- * 由于不适用项目老方法是用的路由策略被抛弃
- */
-//app.config(
-//    ['$routeProvider', function ($routeProvider) {
-//        $routeProvider
-//          .when('/Index', {
-//              templateUrl: 'modules/Index/view/index.html',
-//              data: { title: '首页' },
-//              controller: 'modules/Index/render/index.js'
-//              //resolve: load(['modules/Index/render/index.js'])
-//              //              access:["admin"]
-//          })
-//
-//          .when('/Activity', {
-//              templateUrl: 'modules/activity/view/activity.html',
-//              data: { title: 'activity' }
-//              //              resolve: load(['scripts/controllers/chart.js','scripts/controllers/vectormap.js']),
-//              //              access:["admin"]
-//          })
-//
-//          .otherwise({
-//              redirectTo: 'app/Index'
-//          })
-//
-//        //-----------------------end-------------------
-//
-//    }
-//    ]
-//  );
-
-
-app.config(['$stateProvider', '$urlRouterProvider',function($stateProvider, $urlRouterProvider){
-    $urlRouterProvider
-        .otherwise('/app/home');
-    $stateProvider
-        .state('app',{
-            url:'/app',
-            templateUrl:'Common/widget/nav/nav.html'
-        })
-        .state('app.home',{
-            url:'/home',
-            templateUrl:'modules/Index/view/Index.html'
-        })
         .state('app.setting',{
             url:'/setting',
             templateUrl:'modules/setting/view/setting.html'
@@ -145,10 +62,6 @@ app.config(['$stateProvider', '$urlRouterProvider',function($stateProvider, $url
         .state('app.zhongtian_seller',{
             url:'/zhongtian_seller',
             templateUrl:'modules/zhongtian_seller/view/zhongtian_seller.html'
-        })
-        .state('app.activity',{
-            url:'/activity',
-            templateUrl:'modules/activity/view/activity.html'
         })
         .state('app.partner_list',{
             url:'/partner_list',
