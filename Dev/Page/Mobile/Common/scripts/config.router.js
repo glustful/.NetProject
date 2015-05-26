13a5b7298fadb6be9e3ba150d2ca928eff45d942
杨波 <yb@yunjoy.cn>
2015-05-26 16:07:17 +0800

    增加partner_details.js

--- Dev/Page/Mobile/Common/scripts/config.router.js
+++ Dev/Page/Mobile/Common/scripts/config.router.js
@@ -157,66 +157,12 @@ app.config(['$stateProvider', '$urlRouterProvider',function($stateProvider, $url
             url:'/credit_add',
             templateUrl:'modules/credit_add/view/credit_add.html'
         })
-<<<<<<< .mine
-        .state('app.carry_client',{
-            url:'/carry_client',
-            templateUrl:'modules/carry_client/view/carry_client.html'
-        })
-        .state('app.credit_add',{
-            url:'/credit_add',
-            templateUrl:'modules/credit_add/view/credit_add.html'
-        })
 }]);
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-=======
-        .state('app.task',{
-            url:'/task',
-            templateUrl:'modules/task/view/task.html',
-            resolve: load(['../task.js'])
-
-        })
-        .state('app.nominate',{
-            url:'/nominate',
-            templateUrl:'modules/nominate/view/nominate.html'
-        })
-}]);
 //-----------------------end-------------------
 
 function load(srcs, callback) {
@@ -253,4 +199,3 @@ function load(srcs, callback) {
 
 
 
->>>>>>> .theirs
