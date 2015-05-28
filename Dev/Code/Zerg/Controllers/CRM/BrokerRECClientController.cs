0cd219447d372d7098aa09403709ffa3cb37d98d
yangdingpeng <evesgf@163.com>
2015-05-28 14:07:27 +0800

    新增推荐，新增上访

--- Dev/Code/Zerg/Controllers/CRM/BrokerRECClientController.cs
+++ Dev/Code/Zerg/Controllers/CRM/BrokerRECClientController.cs
@@ -1,4 +1,6 @@
-﻿using CRM.Entity.Model;
+﻿using System.Globalization;
+using System.Web.Http.Results;
+using CRM.Entity.Model;
 using CRM.Service.Broker;
 using CRM.Service.BrokerRECClient;
 using System;
@@ -8,7 +10,11 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using System.Web.Http.Cors;
+using CRM.Service.ClientInfo;
+using YooPoon.Core.Site;
+using YooPoon.WebFramework.MVC;
 using Zerg.Common;
+using Zerg.Models.CRM;
 
 namespace Zerg.Controllers.CRM
 {
@@ -19,10 +25,20 @@ namespace Zerg.Controllers.CRM
     {
          public readonly IBrokerRECClientService _BrokerRECClientService;
          private readonly IBrokerService _brokerService;//经纪人
-         public BrokerRECClientController(IBrokerRECClientService BrokerRECClientService, IBrokerService brokerService)
+         private readonly IClientInfoService _clientInfoService;
+         private readonly IWorkContext _workContext;
+
+         public BrokerRECClientController(
+             IBrokerRECClientService BrokerRECClientService, 
+             IBrokerService brokerService,
+             IClientInfoService clientInfoService,
+             IWorkContext workContext
+             )
          {
              _BrokerRECClientService =BrokerRECClientService;
              _brokerService = brokerService;
+             _clientInfoService = clientInfoService;
+             _workContext = workContext;
          }
 
 
@@ -52,28 +68,55 @@ namespace Zerg.Controllers.CRM
          /// <param name="brokerrecclient"></param>
          /// <returns></returns>
          [HttpPost]
-         public HttpResponseMessage Add([FromBody]  BrokerRECClientEntity  brokerrecclient)
+         public HttpResponseMessage Add([FromBody]  BrokerRECClientModel  brokerrecclient)
          {
-             var entity=new BrokerRECClientEntity
+             //查询客户信息
+             var sech = new ClientInfoSearchCondition
              {
-                 Broker = brokerrecclient.Broker
+                 Clientname = brokerrecclient.Clientname,
+                 Phone = brokerrecclient.Phone.ToString(CultureInfo.InvariantCulture)
+
              };
+             var Cid = _clientInfoService.GetClientInfosByCondition(sech).First().Id;
+             var type = _BrokerRECClientService.GetBrokerRECClientById(Cid).Status;
 
-               try
-                {
-                    if (_BrokerRECClientService.Create(entity) != null)
-                    {
-                        return PageHelper.toJson(PageHelper.ReturnValue(true, "数据添加成功！"));
-                    }
-                }
-                catch
-                {
-                    return PageHelper.toJson(PageHelper.ReturnValue(false, "数据添加失败！"));
-                }
-              return PageHelper.toJson(PageHelper.ReturnValue(false, "数据验证错误！"));
+             //检测
+             if (type!=EnumBRECCType.等待上访)
+             {
+                 //客户信息
+                 var client = new ClientInfoEntity
+                 {
+                     Clientname = brokerrecclient.Clientname,
+                     Phone = brokerrecclient.Phone.ToString(CultureInfo.InvariantCulture),
+                     Housetype = brokerrecclient.HouseType,
+                     Houses = brokerrecclient.Houses,
+                     Note = brokerrecclient.Note,
+                     Adduser = brokerrecclient.Broker,
+                     Addtime = DateTime.Now,
+                     Upuser = brokerrecclient.Broker,
+                     Uptime = DateTime.Now
+                 };
+                 _clientInfoService.Create(client);
 
+                 var cmodel = _clientInfoService.GetClientInfosByCondition(sech).First();
+
+                 var model = new BrokerRECClientEntity
+                 {
+                     Broker = _brokerService.GetBrokerById(brokerrecclient.Broker),
+                     ClientInfo = cmodel,
+                     Phone = brokerrecclient.Phone,
+                     Qq = brokerrecclient.Qq,
+                     Adduser = _workContext.CurrentUser.Id,
+                     Addtime = DateTime.Now,
+                     Upuser = _workContext.CurrentUser.Id,
+                     Uptime = DateTime.Now,
+                     Projectid = brokerrecclient.Projectid,
+                     Status = EnumBRECCType.审核中,
+                 };
+                 _BrokerRECClientService.Create(model);
+                 return PageHelper.toJson(PageHelper.ReturnValue(true, "提交成功"));
+             }
+             return PageHelper.toJson(PageHelper.ReturnValue(false, "该客户正在上访！"));
          }
-
-
     }
 }
