835c7c1f54e0386645506b1af7f13859c0ddd3f4
杨波 <yb@yunjoy.cn>
2015-06-04 17:40:01 +0800

    增加提现页和提现详情页

--- Dev/Page/Mobile/Common/scripts/config.router.js
+++ Dev/Page/Mobile/Common/scripts/config.router.js
@@ -224,6 +224,26 @@ app  .run(
             url:'/chip',
             templateUrl:'modules/chip/view/chip.html'
         })
+        .state('app.chipDetail',{
+            url:'/chipDetail',
+            templateUrl:'modules/chipDetail/view/chipDetail.html'
+        })
+        .state('app.chipPartake',{
+            url:'/chipPartake',
+            templateUrl:'modules/chipPartake/view/chipPartake.html'
+        })
+        .state('app.chipEle',{
+            url:'/chipEle',
+            templateUrl:'modules/chipEle/view/chipEle.html'
+        })
+        .state('app.withdrawals',{
+            url:'/withdrawals',
+            templateUrl:'modules/withdrawals/view/withdrawals.html'
+        })
+        .state('app.withdrawalsDetail',{
+            url:'/withdrawalsDetail',
+            templateUrl:'modules/withdrawalsDetail/view/withdrawalsDetail.html'
+        })
 
 
         function load(srcs, callback) {
