93d95faf0ad8916ef8324081bed6571965aea927
gaofengming <1013882794@qq.com>
2015-06-04 10:57:35 +0800

    注册js

--- Dev/Page/Mobile/modules/Register/controller/RegisterController.js
+++ Dev/Page/Mobile/modules/Register/controller/RegisterController.js
@@ -2,15 +2,22 @@
  * Created by gaofengming on 2015/6/2.
  */
 app.controller('registerController',function($scope,$http){
-    $scope.register={
-        UserType:'æ≠ºÕ»À',
-        Brokername:'zhangsan',
-        UserName:'xiaosan',
-        Phone:'13629609670',
-        Password:'123456'
-    }
-    $scope.registerSubmit=function(){
-        $http.post(SETTING.ApiUrl+'/AdminRecom/AddBroker'+$scope.register).success(function(data){
+
+    $scope.registerSubmit = function(){
+        var usertype = 'ÁªèÁ∫™‰∫∫';
+        var username = document.getElementById("username").value;
+        var userpassword = document.getElementById("userPassword").value;
+        var phoneNumber = document.getElementById("phoneNumber").value;
+        $scope.register={
+            UserType:usertype,
+            Brokername:username,
+            UserName:username,
+            Phone:phoneNumber,
+            Password:userpassword
+        }
+        console.log($scope.register);
+        $http.post(SETTING.ApiUrl+'/AdminRecom/AddBroker',$scope.register,{'withCredentials':true}).success(function(data){
+            console.log(data);
         });
     }
 
