29cadd3ed49d2653c532c1c155ef52807a62727c
彭贵飞 <944015456@qq.com>
2015-06-08 21:28:09 +0800

    product详细信息添加字段

--- Dev/Code/Zerg/Controllers/Trading/Product/ProductController.cs
+++ Dev/Code/Zerg/Controllers/Trading/Product/ProductController.cs
@@ -212,6 +212,8 @@ namespace Zerg.Controllers.Trading.Product
                 Price = product.Price,
                 SubTitle = product.SubTitle,
                 Phone = product.ContactPhone,
+                // ReSharper disable once PossibleNullReferenceException
+                Type = product.ProductParameter.FirstOrDefault(p=>p.Parameter.Name=="户型").ParameterValue.Parametervalue,
                 Advertisement=product.ProductDetail.Ad2,
                 Productimg1 = product.ProductDetail.Productimg1,
                 Productimg2 = product.ProductDetail.Productimg2,
