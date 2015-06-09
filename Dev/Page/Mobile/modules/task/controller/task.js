b589bd7b2033266f694b288745bfff45c63e14c2
黄秀宇 <550206659@qq.com>
2015-06-09 21:14:40 +0800

    改变了下任务样式

--- Dev/Page/Mobile/modules/task/controller/task.js
+++ Dev/Page/Mobile/modules/task/controller/task.js
@@ -68,37 +68,43 @@ app.controller('taskController',['$http','$scope','AuthService',function($http,$
     $scope.warm="";
     $scope.tipp="加载更多。。。"
     //查询任务
-   var page = 1                                //读取的页数
-        , loading = false
-       ,pages=2;                      //判断是否正在读取内容的变量
+   var  loading = false
+       ,pages= 2,total=0;                      //判断是否正在读取内容的变量
     $scope.posts = [];
 
     var pushContent= function() {                    //核心是这个函数，向$scope.posts
         //添加内容
           $scope.searchCondition.type="all";
-            if (!loading && page < pages) {                         //如果页面没有正在读取
+            if (!loading && $scope.searchCondition.page < pages)//如果页面没有正在读取
+            {
                 loading = true;                     //告知正在读取
                 $http.get(SETTING.ApiUrl+'/Task/TaskListMobile/',{params:$scope.searchCondition,'withCredentials':true}).success(function(data) {
-                    if (!data.Status) {
-
-                    pages = data.totalCount / 10 + 1;
-                    console.log(data);
-                    for (var i = 0; i <= data.list.length - 1; i++) {
+                    if (!data.Status)
+                    {
+                        $scope.searchCondition.page++;     //翻页
+                    pages = data.totalCount / 10 + 1;//计算一共几页
+                    total=data.totalCount;//保存任务总数
+                    for (var i = 0; i <= data.list.length - 1; i++)
+                    {
                         $scope.posts.push(data.list[i]);
                     }
                     loading = false;            //告知读取结束
-                } else{
-                        $scope.tipp = "没有任务";
+                        $scope.tipp="加载更多"+$scope.posts.length+"/"+data.totalCount;
+                }
+                    else{
+                        $scope.tipp = "目前没有任务，先去洗洗睡哟";
             }
-                    });
-                page++;                             //翻页
-                if (page > pages) {
-                    $scope.tipp = "没有更多了";
+                    }
+                );
+
+                if ($scope.searchCondition.page > pages)
+                {
+                    $scope.tipp = "没有更多了"+"(共"+total+"条)";
                 }
             }
             else
             {
-                $scope.tipp = "没有更多了";
+                $scope.tipp = "没有更多了"+"(共"+total+"条)";
             }
     };
     pushContent();
