f09d0d20d55cc343a53b0093fbb1fbe84dfd9367
gaofengming <1013882794@qq.com>
2015-06-12 18:06:27 +0800

    修改页面权限

--- Dev/Page/Mobile/Common/scripts/config.router.js
+++ Dev/Page/Mobile/Common/scripts/config.router.js
@@ -12,26 +12,26 @@
  * 由于不适用项目老方法是用的路由策略被抛弃
  */
 app  .run(
-    ['$rootScope', '$state', '$stateParams',
-        function ($rootScope, $state, $stateParams) {
+    ['$rootScope', '$state', '$stateParams','AuthService',
+        function ($rootScope, $state, $stateParams,AuthService) {
             $rootScope.$state = $state;
             $rootScope.$stateParams = $stateParams;
-            //$rootScope.$on('$stateChangeStart', function (event,next) {
-            //    if(next.name==='access.signin' || next.name==='access.signup' || next.name==='access.forgot-password'){
-            //        return;
-            //    }
-            //    if(!AuthService.IsAuthenticated()){
-            //        event.preventDefault();
-            //        $state.go('access.signin');
-            //    }
-            //    if(next.access !== undefined){
-            //        if(!AuthService.IsAuthorized(next.access)){
-            //            event.preventDefault();
-            //            //TODO:跳转到权限提示页
-            //        }
-            //
-            //    }
-            //});
+            $rootScope.$on('$stateChangeStart', function (event,next) {
+                if(next.name==='app.home' || next.name==='user.login' || next.name==='app.storeroom'||next.name==='user.register'||next.name==='user.PasswordFound'){
+                    return;
+                }
+                if(!AuthService.IsAuthenticated()){
+                    event.preventDefault();
+                    $state.go('user.login');
+                }
+                if(next.access !== undefined){
+                    if(!AuthService.IsAuthorized(next.access)){
+                        event.preventDefault();
+                        //TODO:跳转到权限提示页
+                    }
+
+                }
+            });
         }
     ]
 )
@@ -102,13 +102,15 @@ app  .run(
         .state('app.person_setting',{
             url:'/person_setting',
             templateUrl:'modules/person_setting/view/person_setting.html',
-            resolve:load('modules/person_setting/controller/personsettingController.js')
+            resolve:load('modules/person_setting/controller/personsettingController.js'),
+            access:['broker']
         })
 
         .state('app.security_setting',{
             url:'/security_setting',
             templateUrl:'modules/security_setting/view/security_setting.html',
-            resolve:load('modules/security_setting/controller/SecuritySetting.js')
+            resolve:load('modules/security_setting/controller/SecuritySetting.js'),
+            access:['broker']
         })
         .state('app.zhongtian_HouseDetail',{
             url:'/zhongtian_HouseDetail',
