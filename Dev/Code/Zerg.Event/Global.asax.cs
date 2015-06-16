c1399c9f2f80e869c32174019ab3525ff18a3b01
zhangzhuoning <wuweimao@live.com>
2015-06-16 10:34:28 +0800

    版本提交

---
+++ Dev/Code/Zerg.Event/Global.asax.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using System.Web.Security;
+using System.Web.SessionState;
+using YooPoon.Core.Autofac;
+using YooPoon.WebFramework.Dependency;
+using Yunjoy.Web.Admin;
+using Zerg.Event.App_Start;
+using System.Web;
+using System.Web.Http;
+
+
+namespace Zerg.Event
+{
+    public class Global : System.Web.HttpApplication
+    {
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+
+            //注册依赖容器
+            var initialize = new InitializeContainer();
+            initialize.Initializing();
+
+            var resolver = new YpDependencyResolver(initialize);
+            DependencyResolver.SetResolver(resolver);
+
+            //注册全局Filter
+            //FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+
+            GlobalConfiguration.Configuration.EnableCors();
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+        }
+
+        //protected void Session_Start(object sender, EventArgs e)
+        //{
+
+        //}
+
+        //protected void Application_BeginRequest(object sender, EventArgs e)
+        //{
+
+        //}
+
+        //protected void Application_AuthenticateRequest(object sender, EventArgs e)
+        //{
+
+        //}
+
+        //protected void Application_Error(object sender, EventArgs e)
+        //{
+
+        //}
+
+        //protected void Session_End(object sender, EventArgs e)
+        //{
+
+        //}
+
+        //protected void Application_End(object sender, EventArgs e)
+        //{
+
+        //}
+    }
+}
