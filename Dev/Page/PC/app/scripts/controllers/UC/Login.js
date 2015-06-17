38c654236fecffe493b014e076748fb1092d7331
gaofengming <1013882794@qq.com>
2015-06-17 15:15:55 +0800

    删除后台多余内容，以及修改后台部份bug

--- Dev/Page/PC/app/scripts/controllers/UC/Login.js
+++ Dev/Page/PC/app/scripts/controllers/UC/Login.js
@@ -4,7 +4,7 @@
 angular.module('app').controller('LoginControl',['$scope','$state','AuthService',function($scope,$state,AuthService){
     $scope.Login = function(){
         AuthService.doLogin($scope.user.name,$scope.user.password,function(){
-            $state.go('page.settings');
+            $state.go('page.Trading.product.product');
         },function(){
             //todo:失败时候显示错误信息
         })
