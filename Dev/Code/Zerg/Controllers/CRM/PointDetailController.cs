bccd3ea447b6af169b96df7724d538ffd1fa0e76
YANGHUI XU <xuyanghui.china@gmail.com>
2015-06-23 11:38:06 -0700

    新增类和方法说明

--- Dev/Code/Zerg/Controllers/CRM/PointDetailController.cs
+++ Dev/Code/Zerg/Controllers/CRM/PointDetailController.cs
@@ -9,38 +9,47 @@ using System.Net.Http;
 using System.Web.Http;
 using System.Web.Http.Cors;
 using Zerg.Common;
+using System.ComponentModel;
 
 namespace Zerg.Controllers.CRM
 {
-     [EnableCors("*", "*", "*", SupportsCredentials = true)]
+    [EnableCors("*", "*", "*", SupportsCredentials = true)]
     /// <summary>
     /// 积分详情管理  李洪亮  2015-05-05
     /// </summary>
+    [Description("积分管理类")]
     public class PointDetailController : ApiController
     {
 
         private IPointDetailService _pointdetailService;
         private readonly IBrokerService _brokerService;
+        /// <summary>
+        /// 积分管理初始化
+        /// </summary>
+        /// <param name="pointdetailService">pointdetailService</param>
+        /// <param name="brokerService">brokerService</param>
         public PointDetailController(IPointDetailService pointdetailService, IBrokerService brokerService)
         {
-            _pointdetailService =pointdetailService;
-            _brokerService =brokerService;
+            _pointdetailService = pointdetailService;
+            _brokerService = brokerService;
         }
 
         #region 积分详情
 
-      /// <summary>
-      /// 查询经纪人的积分详情
-      /// </summary>
-      /// <param name="userId">经纪人ID</param>
-      /// <returns></returns>
-
+        /// <summary>
+        /// 传入经纪人ID，查询经纪人积分详情，返回经纪人积分想详情
+        /// </summary>
+        /// <param name="userId">经纪人ID</param>
+        /// <param name="page">页码</param>
+        /// <param name="pageSize">页面数量</param>
+        /// <returns>经纪人积分详情</returns>
+        [Description("查询返回经纪人积分详情")]
         [System.Web.Http.HttpGet]
         public HttpResponseMessage GetPointDetailByUserId(string userId = null, int page = 1, int pageSize = 10)
         {
             var pointdetailCon = new PointDetailSearchCondition
             {
-               Brokers=_brokerService.GetBrokerById(Convert.ToInt32(userId)),
+                Brokers = _brokerService.GetBrokerById(Convert.ToInt32(userId)),
                 Page = Convert.ToInt32(page),
                 PageCount = pageSize
             };
@@ -52,35 +61,37 @@ namespace Zerg.Controllers.CRM
                 p.Pointsds,
                 p.Addtime
 
-            }).ToList().Select(p => new {
+            }).ToList().Select(p => new
+            {
                 Id = p.Id,
                 p.Addpoints,
                 p.Pointsds,
-               Addtime= p.Addtime.ToString ("yyyy-MM-dd")
-            
+                Addtime = p.Addtime.ToString("yyyy-MM-dd")
+
             });
             var PointDetailListCount = _pointdetailService.GetPointDetailCount(pointdetailCon);
             return PageHelper.toJson(new { List = PointDetailList, Condition = pointdetailCon, totalCount = PointDetailListCount });
-                  
+
         }
 
         /// <summary>
-        /// 新增积分详情
+        /// 传入积分参数，新增积分详情，返回新增积分详情结果状态信息
         /// </summary>
-        /// <param name="name"></param>
-        /// <returns></returns>
+        /// <param name="pointDetail">积分参数</param>
+        /// <returns>新增结果状态信息</returns>
+        [Description("查询返回经纪人积分详情")]
         [System.Web.Http.HttpPost]
         public HttpResponseMessage AddPointDetail([FromBody] PointDetailEntity pointDetail)
         {
 
-            if (pointDetail!=null)
+            if (pointDetail != null)
             {
                 var entity = new PointDetailEntity
                 {
-                    Addpoints=pointDetail.Addpoints,
-                    Broker=null,
-                    Pointsds=pointDetail.Pointsds,
-                    Totalpoints=pointDetail.Totalpoints,
+                    Addpoints = pointDetail.Addpoints,
+                    Broker = null,
+                    Pointsds = pointDetail.Pointsds,
+                    Totalpoints = pointDetail.Totalpoints,
                     Uptime = DateTime.Now,
                     Addtime = DateTime.Now,
 
