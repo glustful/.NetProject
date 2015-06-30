275f48123a42542545ee6df990c09cc125a23d2f
YANGHUI XU <xuyanghui.china@gmail.com>
2015-06-30 19:40:38 +0800

    添加了房源功能，能列表显示出房源。

--- Dev/Android/yoopoon/src/com/yoopoon/home/ui/home/FramHouseFragment.java
+++ Dev/Android/yoopoon/src/com/yoopoon/home/ui/home/FramHouseFragment.java
@@ -1,22 +1,163 @@
 package com.yoopoon.home.ui.home;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 import org.androidannotations.annotations.AfterInject;
 import org.androidannotations.annotations.EFragment;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.text.format.DateUtils;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+
+import com.handmark.pulltorefresh.library.PullToRefreshBase;
+import com.handmark.pulltorefresh.library.PullToRefreshListView;
 import com.yoopoon.home.R;
+import com.yoopoon.home.data.net.ProgressMessage;
+import com.yoopoon.home.data.net.RequestAdapter;
+import com.yoopoon.home.data.net.ResponseData;
+import com.yoopoon.home.data.net.RequestAdapter.RequestMethod;
+import com.yoopoon.home.data.net.ResponseData.ResultState;
+import com.yoopoon.home.ui.active.ActiveBrandAdapter;
+import com.yoopoon.home.ui.home.FramActiveFragment.HowWillIrefresh;
 
 @EFragment(R.layout.home_fram_house_fragment)
-public class FramHouseFragment extends FramSuper{
-	public static FramHouseFragment instance = null;
+/*@EFragment()*/
+public class FramHouseFragment extends FramSuper {
+	public static final String LOGTAG = "FramHouseFragment";
+	View rootView;
+	HashMap<String, String> parameter;
+	ArrayList<JSONObject> mJsonObjects;
+	PullToRefreshListView listView;
+	public static FramHouseFragment instance;
 
+	public static FramHouseFragment getInstance() {
+		return instance;
+	}
 
-@AfterInject
-void afterInject(){
-	instance = this;
-	
-}
+	Context mContext;
+	ListView refreshView;
+	HouseInfoAdapter mHouseInfoAdapter;
 
+	@Override
+	@Nullable
+	public View onCreateView(LayoutInflater inflater,
+			@Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+		Log.e(LOGTAG, "Fragment oncreateView");
 
+		if (rootView != null) {
+			ViewGroup parent = (ViewGroup) rootView.getParent();
+			if (null != parent) {
+				parent.removeView(rootView);
+			}
+		} else {
+			rootView = LayoutInflater.from(getActivity()).inflate(
+					R.layout.home_fram_house_fragment, null);
+			listView = (PullToRefreshListView) rootView
+					.findViewById(R.id.matter_list_view);
+			instance = this;
+			mJsonObjects = new ArrayList<JSONObject>();
+			initParameter();
+			mContext = getActivity();
+			initViews();
+		}
 
+		return rootView;
+	}
+
+	void initViews() {
+
+		listView.setOnRefreshListener(new HowWillIrefresh());
+		listView.setMode(PullToRefreshBase.Mode.DISABLED);
+		refreshView = listView.getRefreshableView();
+
+		refreshView.setFastScrollEnabled(false);
+		refreshView.setFadingEdgeLength(0);
+		mHouseInfoAdapter = new HouseInfoAdapter(mContext);
+		refreshView.setAdapter(mHouseInfoAdapter);
+
+		requestHouseList();
+	}
+
+	private void initParameter() {
+		if (parameter == null) {
+			parameter = new HashMap<String, String>();
+		}
+
+		parameter.clear();
+		parameter.put("AreaName", "");
+		parameter.put("OrderBy", "OrderByAddtime");
+		parameter.put("IsDescending", "true");
+		parameter.put("Page", "1");
+		parameter.put("PageCount", "10");
+		parameter.put("PriceBegin", "");
+		parameter.put("PriceEnd", "");
+		parameter.put("TypeId", "");
+	}
+
+	@AfterInject
+	void afterInject() {
+
+	}
+
+	private void requestHouseList() {
+		new RequestAdapter() {
+
+			@Override
+			public void onReponse(ResponseData data) {
+
+				if (data.getResultState() == ResultState.eSuccess) {
+
+					JSONArray list = data.getMRootData().optJSONArray("List");
+					if (list == null || list.length() < 1)
+						return;
+					for (int i = 0; i < list.length(); i++) {
+						mJsonObjects.add(list.optJSONObject(i));
+					}
+					mHouseInfoAdapter.refresh(mJsonObjects);
+				}
+			}
+
+			@Override
+			public void onProgress(ProgressMessage msg) {
+				// TODO Auto-generated method stub
+
+			}
+		}.setUrl(getString(R.string.url_product_search))
+				.setRequestMethod(RequestMethod.eGet)
+
+				.addParam(parameter).notifyRequest();
+
+	}
+
+	class HowWillIrefresh implements
+			PullToRefreshBase.OnRefreshListener2<ListView> {
+
+		@Override
+		public void onPullDownToRefresh(PullToRefreshBase<ListView> refreshView) {
+			String label = DateUtils.formatDateTime(getActivity(),
+					System.currentTimeMillis(), DateUtils.FORMAT_SHOW_TIME
+							| DateUtils.FORMAT_SHOW_DATE
+							| DateUtils.FORMAT_ABBREV_ALL);
+			refreshView.getLoadingLayoutProxy().setLastUpdatedLabel(label);
+
+		}
+
+		@Override
+		public void onPullUpToRefresh(PullToRefreshBase<ListView> refreshView) {
+
+		}
+
+	}
 
 }
---
+++ Dev/Android/yoopoon/src/com/yoopoon/home/ui/home/HouseInfoAdapter.java
@@ -0,0 +1,124 @@
+package com.yoopoon.home.ui.home;
+
+import java.util.ArrayList;
+
+import org.json.JSONObject;
+
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.yoopoon.home.MyApplication;
+import com.yoopoon.home.R;
+
+import android.app.Activity;
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+/**
+ * 房源中实体对应的Adapter
+ * 
+ * @author king
+ *
+ */
+public class HouseInfoAdapter extends BaseAdapter {
+  
+	Context mContext;
+	ArrayList<JSONObject> datas;
+	int height = 0;
+
+	public HouseInfoAdapter(Context mContext) {
+		this.mContext = mContext;
+		datas = new ArrayList<JSONObject>();
+		height = MyApplication.getInstance().getDeviceInfo((Activity) mContext).heightPixels / 6;
+	}
+
+	@Override
+	public int getCount() {
+		return datas.size();
+	}
+
+	@Override
+	public Object getItem(int position) {
+		return datas.get(position);
+	}
+
+	@Override
+	public long getItemId(int position) {
+		return position;
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		ViewHandler viewHandler;
+		if (convertView == null) {
+			convertView = LayoutInflater.from(mContext).inflate(
+					R.layout.home_house_listview_item, null);
+			viewHandler = new ViewHandler();
+			viewHandler.init(convertView);
+			convertView.setTag(viewHandler);
+		} else {
+			viewHandler = (ViewHandler) convertView.getTag();
+		}
+
+		JSONObject item = datas.get(position);
+		String url = mContext.getString(R.string.url_host_img)
+				+ item.optString("Productimg");
+		 
+		viewHandler.houseImageView
+				.setLayoutParams(new LinearLayout.LayoutParams(
+						LinearLayout.LayoutParams.MATCH_PARENT, height));
+		viewHandler.houseImageView.setTag(url);
+		/*viewHandler.houseImageView.setImageResource(R.drawable.ic_launcher);
+		if (viewHandler.houseImageView.getTag() != null && viewHandler.houseImageView.getTag().equals(url)) {
+			
+		}*/
+		ImageLoader.getInstance().displayImage(url, viewHandler.houseImageView,
+				MyApplication.getOptions(), MyApplication.getLoadingListener());
+		viewHandler.houseSubtitleTextView.setText(item.optString("SubTitle"));
+		
+
+		viewHandler.houseProductnameTextView.setText(item
+				.optString("Productname"));
+		viewHandler.housePriceTextView.setText(item.optString("Price")+"¥/m²");
+		viewHandler.houseTypeAcreaqeStockRuleTextView.setText(item
+				.optString("Type")
+				+ "/"
+				+ item.optString("Acreage")+"m²"
+				+ "/"
+				+ "在售"+item.optString("StockRule")+"套");
+		return convertView;
+	}
+
+	private class ViewHandler {
+		private TextView houseSubtitleTextView;
+		private ImageView houseImageView;
+		private TextView houseProductnameTextView;
+		private TextView housePriceTextView;
+		private TextView houseTypeAcreaqeStockRuleTextView;
+
+		void init(View root) {
+			houseSubtitleTextView = (TextView) root
+					.findViewById(R.id.house_subtitle);
+			houseImageView = (ImageView) root.findViewById(R.id.house_image);
+			houseProductnameTextView = (TextView) root
+					.findViewById(R.id.house_productname);
+			housePriceTextView = (TextView) root.findViewById(R.id.house_price);
+			houseTypeAcreaqeStockRuleTextView = (TextView) root
+					.findViewById(R.id.tpye_area_stockRule);
+
+		}
+	}
+
+	public void refresh(ArrayList<JSONObject> mJsonObjects) {
+		datas.clear();
+		datas.addAll(mJsonObjects);
+		this.notifyDataSetChanged();
+	}
+
+}
