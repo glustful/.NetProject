832a1762e4ac2d8751ad1c71c8c8f9e3e43fc6ba
彭贵飞 <944015456@qq.com>
2015-06-30 19:41:45 +0800

    查询优惠券

--- Dev/Code/Zerg.Event/Controllers/Coupons/CouponsController.cs
+++ Dev/Code/Zerg.Event/Controllers/Coupons/CouponsController.cs
@@ -3,16 +3,49 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Event.Entity.Entity.Coupon;
+using Event.Service.Coupon;
+using Trading.Service.ProductBrand;
+using Zerg.Event.Models.Coupons;
 
 namespace Zerg.Event.Controllers.Coupons
 {
     public class CouponsController : Controller
     {
-        //
-        // GET: /Coupons/
+        private readonly ICouponCategoryService _couponCategoryService;
+        private readonly IProductBrandService _productBrandService;
+
+        public CouponsController(ICouponCategoryService couponCategoryService, IProductBrandService productBrandService)
+        {
+            _couponCategoryService = couponCategoryService;
+            _productBrandService = productBrandService;
+        }
         public ActionResult coupons()
         {
-            return View();
+            var ConConditon=new CouponCategorySearchCondition
+            {
+                OrderBy = EnumCouponCategorySearchOrderBy.OrderById
+            };
+            var list = _couponCategoryService.GetCouponCategoriesByCondition(ConConditon).Select(p=>new
+            {
+                p.Id,
+                p.BrandId,
+                p.Name,
+                p.ReMark,
+                p.Price,
+                p.Count
+            }).ToList().Select(pp=>new CouponCategoryModel
+            {
+                Id = pp.Id,
+                Name = pp.Name,
+                Price = pp.Price,
+                Count = pp.Count,
+                ReMark = pp.ReMark,
+                BrandImg = _productBrandService.GetProductBrandById(pp.BrandId).Bimg,
+                SubTitle = _productBrandService.GetProductBrandById(pp.BrandId).SubTitle,
+                ProductParamater = _productBrandService.GetProductBrandById(pp.BrandId).ParameterEntities.ToDictionary(k => k.Parametername, v => v.Parametervaule)
+            });
+            return View(list);
         }
 	}
 }
