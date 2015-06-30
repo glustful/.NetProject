30b189682f406875617d072e11cadbed26edff09
段玥旻 <atawat@live.cn>
2015-06-30 16:09:21 +0800

    添加优惠券实体

---
+++ Dev/Code/Event.Entity/Entity/Coupon/CouponCategory.cs
@@ -0,0 +1,12 @@
+﻿namespace Event.Entity.Entity.Coupon
+{
+    public class CouponCategory
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ReMark { get; set; }
+        public int Count { get; set; }
+        public int BrandId { get; set; }
+        public string Price { get; set; }
+    }
+}
