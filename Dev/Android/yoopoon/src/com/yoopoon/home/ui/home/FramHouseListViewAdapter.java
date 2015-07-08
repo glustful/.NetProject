307d17ce89f328e4e4dfe0eb798f8e6448540cb5
YANGHUI XU <xuyanghui.china@gmail.com>
2015-07-08 09:02:22 +0800

    修改了房源页顶端条件点击事件

--- Dev/Android/yoopoon/src/com/yoopoon/home/ui/home/FramHouseListViewAdapter.java
+++ Dev/Android/yoopoon/src/com/yoopoon/home/ui/home/FramHouseListViewAdapter.java
@@ -1,18 +1,11 @@
 package com.yoopoon.home.ui.home;
 
-import java.security.PublicKey;
 import java.util.ArrayList;
 
 import org.json.JSONObject;
 
-import com.nostra13.universalimageloader.core.ImageLoader;
-import com.yoopoon.home.MyApplication;
-import com.yoopoon.home.R;
-import com.yoopoon.home.ui.product.ProductDetailActivity_;
-
 import android.app.Activity;
 import android.content.Context;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -21,17 +14,18 @@ import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.LinearLayout.LayoutParams;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
+
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.yoopoon.home.MyApplication;
+import com.yoopoon.home.R;
+import com.yoopoon.home.ui.product.ProductDetailActivity_;
 
 /**
  * 房源中实体对应的Adapter
- * 
  * @author king
- *
  */
 public class FramHouseListViewAdapter extends BaseAdapter {
-
 	Context mContext;
 	ArrayList<JSONObject> datas;
 	int height = 0;
@@ -41,101 +35,71 @@ public class FramHouseListViewAdapter extends BaseAdapter {
 		datas = new ArrayList<JSONObject>();
 		height = MyApplication.getInstance().getDeviceInfo((Activity) mContext).heightPixels / 6;
 	}
-
 	@Override
 	public int getCount() {
 		return datas.size();
 	}
-
 	@Override
 	public Object getItem(int position) {
 		return datas.get(position);
 	}
-
 	@Override
 	public long getItemId(int position) {
 		return position;
 	}
-
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
 		ViewHandler viewHandler;
 		if (convertView == null) {
-			convertView = LayoutInflater.from(mContext).inflate(
-					R.layout.home_fram_house_listview_item, null);
-			LinearLayout listViewLinearLayout=(LinearLayout) convertView.findViewById(R.id.house_listview_item_linearlayout);
-			LayoutParams layoutParams=(LayoutParams) listViewLinearLayout.getLayoutParams();
-			int screenWidth=mContext.getResources().getDisplayMetrics().widthPixels;
-		 
-			
-			
-			layoutParams.height=screenWidth/3;
+			convertView = LayoutInflater.from(mContext).inflate(R.layout.home_fram_house_listview_item, null);
+			LinearLayout listViewLinearLayout = (LinearLayout) convertView
+					.findViewById(R.id.house_listview_item_linearlayout);
+			LayoutParams layoutParams = (LayoutParams) listViewLinearLayout.getLayoutParams();
+			int screenWidth = mContext.getResources().getDisplayMetrics().widthPixels;
+			layoutParams.height = screenWidth / 3;
 			listViewLinearLayout.setLayoutParams(layoutParams);
-			
 			viewHandler = new ViewHandler();
 			viewHandler.init(convertView);
 			convertView.setTag(viewHandler);
 		} else {
 			viewHandler = (ViewHandler) convertView.getTag();
 		}
-
 		final JSONObject item = datas.get(position);
-		String url = mContext.getString(R.string.url_host_img)
-				+ item.optString("Productimg");
-
-		viewHandler.houseImageView
-				.setLayoutParams(new LinearLayout.LayoutParams(
-						LinearLayout.LayoutParams.MATCH_PARENT, height));
+		String url = mContext.getString(R.string.url_host_img) + item.optString("Productimg");
+		viewHandler.houseImageView.setLayoutParams(new LinearLayout.LayoutParams(
+				LinearLayout.LayoutParams.MATCH_PARENT, height));
 		viewHandler.houseImageView.setTag(url);
-
-		ImageLoader.getInstance().displayImage(url, viewHandler.houseImageView,
-				MyApplication.getOptions(), MyApplication.getLoadingListener());
-		 
-		//viewHandler.houseImageView.
-	 
-		
-		
-		viewHandler.houseProductnameTextView.setText(item
-				.optString("Productname"));
-		viewHandler.housePriceTextView
-				.setText(item.optString("Price") + "元/m²");
-		viewHandler.houseTypeAcreaqeStockRuleTextView.setText(item
-				.optString("Type")
-				+ "/"
-				+ item.optString("Acreage")
-				+ "m²"
-				+ "/" + "在售" + item.optString("StockRule") + "套");
+		ImageLoader.getInstance().displayImage(url, viewHandler.houseImageView, MyApplication.getOptions(),
+				MyApplication.getLoadingListener());
+		// viewHandler.houseImageView.
+		viewHandler.houseProductnameTextView.setText(item.optString("Productname"));
+		viewHandler.housePriceTextView.setText(item.optString("Price") + "元/m²");
+		viewHandler.houseTypeAcreaqeStockRuleTextView.setText(item.optString("Type") + "/" + item.optString("Acreage")
+				+ "m²" + "/" + "在售" + item.optString("StockRule") + "套");
 		viewHandler.houseAdvertisementTextView.setText(item.optString("Advertisement"));
+		// 添加点击事件
 		convertView.setOnClickListener(new OnClickListener() {
-			
 			@Override
 			public void onClick(View v) {
-				ProductDetailActivity_.intent(mContext)
-				.productId(item.optString("Id"))
-				.start();
-				
+				ProductDetailActivity_.intent(mContext).productId(item.optString("Id")).start();
 			}
 		});
 		return convertView;
 	}
 
 	private class ViewHandler {
-		
 		private ImageView houseImageView;
 		private TextView houseProductnameTextView;
 		private TextView housePriceTextView;
 		private TextView houseTypeAcreaqeStockRuleTextView;
 		private TextView houseAdvertisementTextView;
-		
 
 		void init(View root) {
 			houseImageView = (ImageView) root.findViewById(R.id.house_image);
-			houseProductnameTextView = (TextView) root
-					.findViewById(R.id.house_productname);
+			houseProductnameTextView = (TextView) root.findViewById(R.id.house_productname);
 			housePriceTextView = (TextView) root.findViewById(R.id.house_price);
-			houseTypeAcreaqeStockRuleTextView = (TextView) root
-					.findViewById(R.id.tpye_area_stockRule);
-			houseAdvertisementTextView=(TextView)root.findViewById(R.id.house_advertisement);
+			houseTypeAcreaqeStockRuleTextView = (TextView) root.findViewById(R.id.tpye_area_stockRule);
+			houseAdvertisementTextView = (TextView) root.findViewById(R.id.house_advertisement);
 		}
 	}
 
@@ -144,5 +108,4 @@ public class FramHouseListViewAdapter extends BaseAdapter {
 		datas.addAll(mJsonObjects);
 		this.notifyDataSetChanged();
 	}
-
 }
