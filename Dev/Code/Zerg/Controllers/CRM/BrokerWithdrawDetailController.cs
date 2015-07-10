a1889c05dd0c6b42fcf6e125054a817e9ef2ae71
chen <944845129@qq.com>
2015-07-10 18:04:53 +0800

    打款

--- Dev/Code/Zerg/Controllers/CRM/BrokerWithdrawDetailController.cs
+++ Dev/Code/Zerg/Controllers/CRM/BrokerWithdrawDetailController.cs
@@ -16,6 +16,7 @@ using YooPoon.Core.Site;
 using YooPoon.WebFramework.User.Entity;
 using Zerg.Common;
 using System.ComponentModel;
+using System.Text;
 
 namespace Zerg.Controllers.CRM
 {
@@ -82,13 +83,19 @@ namespace Zerg.Controllers.CRM
             }).ToList().Select(a =>new
             {
                 a.Id,
-                a.Withdrawnum,
-                a.BrokeAccount_Id,
+                a.Withdrawnum,             
                 a.Type,
                 WithdrawDesc = a.WithdrawDesc,
                 Withdrawtime = a.Withdrawtime.ToString("yyy-MM-dd"),
             });
-            return PageHelper.toJson(new { List = list });
+            //取出所有明细的ID
+            StringBuilder SB = new StringBuilder();
+            foreach(var p in list)
+            {
+                SB.Append(p.Id.ToString() + ",");
+            }
+
+            return PageHelper.toJson(new { List = list,Ids=SB.ToString() });
         }
         #region 经纪人提现明细详情
 
