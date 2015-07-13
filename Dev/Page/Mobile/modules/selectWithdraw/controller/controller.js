73e987263dba09830095ac5b65038c609b98bc21
chen <944845129@qq.com>
2015-07-13 17:13:44 +0800

    修改

--- Dev/Page/Mobile/modules/selectWithdraw/controller/controller.js
+++ Dev/Page/Mobile/modules/selectWithdraw/controller/controller.js
@@ -1,3 +1,20 @@
 /**
  * Created by chen on 2015/7/13.
  */
+app.controller('withdrawController',['$http','$scope','AuthService',function($http,$scope,AuthService) {
+    $scope.Withdraw={
+      UserId:""
+    };
+
+
+    $scope.currentUser=AuthService.CurrentUser();
+    $scope.Withdraw.UserId = $scope.currentUser.UserId;
+
+    var getwithdrawResult  = function() {
+        $http.get(SETTING.ApiUrl+'/BrokeAccount/GetBrokeAccountByUserId/',{params:$scope.Withdraw,'withCredentials':true}).success(function(data){
+            $scope.list=data.List;
+        });
+    };
+    $scope.getlist=getwithdrawResult;
+
+}])
