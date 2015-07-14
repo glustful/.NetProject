d6992433d201c316a2a04ddb1e9c50798642096a
杨定鹏 <evesgf@163.com>
2015-07-14 14:12:45 +0800

    提交更改

--- Dev/Page/Mobile/app.js
+++ Dev/Page/Mobile/app.js
@@ -7,7 +7,8 @@ var app = angular.module('zergApp', [ 'ui.router','ngCookies','oc.lazyLoad','ngS
 var SETTING = {
     BaseUrl:'http://192.168.1.199/',
     ApiUrl:'http://localhost:50597/api',
-    ImgUrl:'http://img.yoopoon.com/'
+    ImgUrl:'http://img.yoopoon.com/',
+    EventUrl:'http://localhost:16857//api'
 };
 //var SETTING = {
 //    BaseUrl:'http://api.iyookee.cn/',
