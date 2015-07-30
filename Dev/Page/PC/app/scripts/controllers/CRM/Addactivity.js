b4fe193e54c6f8d63bebe939cef0ee3b06ad4f4b
段玥旻 <atawat@live.cn>
2015-07-30 16:32:42 +0800

    删除垃圾代码

--- Dev/Page/PC/app/scripts/controllers/CRM/Addactivity.js
+++ Dev/Page/PC/app/scripts/controllers/CRM/Addactivity.js
@@ -10,7 +10,12 @@ app.controller('addController',['$scope','$http','$state',function($scope,$http,
     }
     $scope.add=function(){
 
-        $http.post(SETTING.ApiUrl+'/Event/AddEvent',$scope.list,{ 'withCredentials':true}).success(function(data){
+        $http.post(SETTING.ApiUrl+'/Event/AddEvent',{
+            EventContent:$scope.list.EventContent,
+            Starttime:$scope.list.Starttime.toLocaleDateString() ,
+            Endtime:$scope.list.Endtime.toLocaleDateString() ,
+            State:'false'
+        },{ 'withCredentials':true}).success(function(data){
             if(data.Status){
                 console.log(data);
                 $state.go("page.CRM.activity.activityList");
--- Dev/Page/PC/app/scripts/controllers/Trading/Parameter.js
+++ Dev/Page/PC/app/scripts/controllers/Trading/Parameter.js
@@ -173,36 +173,3 @@ app.controller('ParameterController', ['$scope', '$http', '$state','$modal', fun
         });
     };
 }]);
-app.filter('dateFilter',function(){
-    return function(date){
-        return FormatDate(date);
-    }
-})
-
-function FormatDate(JSONDateString) {
-    jsondate = JSONDateString.replace("/Date(", "").replace(")/", "");
-    if (jsondate.indexOf("+") > 0) {
-        jsondate = jsondate.substring(0, jsondate.indexOf("+"));
-    }
-    else if (jsondate.indexOf("-") > 0) {
-        jsondate = jsondate.substring(0, jsondate.indexOf("-"));
-    }
-
-    var date = new Date(parseInt(jsondate, 10));
-    var month = date.getMonth() + 1 < 10 ? "0" + (date.getMonth() + 1) : date.getMonth() + 1;
-    var currentDate = date.getDate() < 10 ? "0" + date.getDate() : date.getDate();
-
-    return date.getFullYear()
-        + "-"
-        + month
-        + "-"
-        + currentDate
-//        + "-"
-//        + date.getHours()
-//        + ":"
-//        + date.getMinutes()
-//        + ":"
-//        + date.getSeconds()
-        ;
-
-}
