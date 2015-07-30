b3194aaf4f0d171e7662f755d22b2822b76747da
YANGHUI XU <xuyanghui.china@gmail.com>
2015-07-30 10:57:02 +0800

    添加经纪人在品牌详情页面的我要推荐，我要带客，咨询热线功能

--- Dev/Android/yoopoon/src/com/yoopoon/home/ui/active/BrandDetailActivity.java
+++ Dev/Android/yoopoon/src/com/yoopoon/home/ui/active/BrandDetailActivity.java
@@ -8,9 +8,12 @@ import org.androidannotations.annotations.ViewById;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import android.graphics.Color;
 import android.util.Log;
 import android.view.View;
 import android.widget.LinearLayout;
+
 import com.etsy.android.grid.StaggeredGridView;
 import com.yoopoon.common.base.utils.StringUtils;
 import com.yoopoon.home.MainActionBarActivity;
@@ -26,31 +29,28 @@ public class BrandDetailActivity extends MainActionBarActivity {
 	private static final String TAG = "BrandDetailActivity";
 	@Extra
 	String mJson;
-
 	@ViewById(R.id.grid_view)
 	StaggeredGridView mListView;
 	@ViewById(R.id.ll_progress)
 	LinearLayout ll_progress;
-
 	JSONObject jsonObject;
 	@Bean
 	BrandDetailAdapter mAdapter;
 	BrandDetailHeaderView mBrandDetailHeaderView;
 	int divider = 5;
-
+	
 	@AfterViews
 	void initView() {
 		backButton.setVisibility(View.VISIBLE);
 		backButton.setText("返回");
+		backButton.setTextColor(Color.WHITE);
 		titleButton.setVisibility(View.VISIBLE);
 		titleButton.setText("品牌详情");
-
 		mBrandDetailHeaderView = BrandDetailHeaderView_.build(this);
 		try {
 			jsonObject = new JSONObject(mJson);
 			Log.i(TAG, jsonObject.toString());
 			mBrandDetailHeaderView.init(jsonObject);
-
 		} catch (JSONException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -58,18 +58,14 @@ public class BrandDetailActivity extends MainActionBarActivity {
 		initRecyclerView();
 		requestProductByBrandId();
 	}
-
 	private void requestProductByBrandId() {
 		if (jsonObject == null)
 			return;
 		ll_progress.setVisibility(View.VISIBLE);
 		new RequestAdapter() {
-
 			@Override
 			public void onReponse(ResponseData data) {
-
 				if (data.getResultState() == ResultState.eSuccess) {
-
 					if (data.getMRootData() != null) {
 						JSONObject contentObj = data.getMRootData().optJSONObject("content");
 						if (contentObj != null) {
@@ -77,57 +73,40 @@ public class BrandDetailActivity extends MainActionBarActivity {
 							if (!StringUtils.isEmpty(content))
 								mBrandDetailHeaderView.setContent(content);
 						}
-
 						JSONArray array = data.getMRootData().optJSONArray("productList");
 						if (array != null) {
 							mAdapter.setData(array);
 						}
 						ll_progress.setVisibility(View.GONE);
-
 					}
-
 				}
-
 			}
-
 			@Override
 			public void onProgress(ProgressMessage msg) {
 				// TODO Auto-generated method stub
-
 			}
 		}.setUrl(getString(R.string.url_product_GetProductsByBrand)).setRequestMethod(RequestMethod.eGet)
 				.addParam("BrandId", jsonObject.optString("Id")).notifyRequest();
-
 	}
-
 	private void initRecyclerView() {
 		mListView.addHeaderView(mBrandDetailHeaderView);
 		mListView.setAdapter(mAdapter);
-
 	}
-
 	@Override
 	public void backButtonClick(View v) {
 		this.finish();
-
 	}
-
 	@Override
 	public void titleButtonClick(View v) {
 		// TODO Auto-generated method stub
-
 	}
-
 	@Override
 	public void rightButtonClick(View v) {
 		// TODO Auto-generated method stub
-
 	}
-
 	@Override
 	public Boolean showHeadView() {
 		// TODO Auto-generated method stub
 		return true;
 	}
-
 }
--- Dev/Android/yoopoon/src/com/yoopoon/home/ui/active/BrandDetailAdapter.java
+++ Dev/Android/yoopoon/src/com/yoopoon/home/ui/active/BrandDetailAdapter.java
@@ -1,10 +1,12 @@
 package com.yoopoon.home.ui.active;
 
 import java.util.ArrayList;
+
 import org.androidannotations.annotations.Background;
 import org.androidannotations.annotations.EBean;
 import org.json.JSONArray;
 import org.json.JSONObject;
+
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -13,6 +15,7 @@ import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.TextView;
+
 import com.etsy.android.grid.util.DynamicHeightImageView;
 import com.nostra13.universalimageloader.core.ImageLoader;
 import com.yoopoon.common.base.Tools;
@@ -27,19 +30,17 @@ public class BrandDetailAdapter extends BaseAdapter {
 	private JSONArray data;// 填充数据的集合
 	Context mContext;
 	ArrayList<String> names;
-
+	
 	public BrandDetailAdapter(Context mContext) {
 		super();
 		this.mContext = mContext;
 		names = new ArrayList<String>();
 	}
-
 	public void setData(JSONArray data) {
 		this.data = data;
 		this.notifyDataSetChanged();
 		initImgUrl();
 	}
-
 	@Background
 	void initImgUrl() {
 		if (data == null)
@@ -49,7 +50,7 @@ public class BrandDetailAdapter extends BaseAdapter {
 			names.add(data.optJSONObject(i).optString("Productimg"));
 		}
 	}
-
+	
 	// 创建一个viewholder类
 	public static class ViewHolder extends RecyclerView.ViewHolder {
 		DynamicHeightImageView img;
@@ -57,7 +58,7 @@ public class BrandDetailAdapter extends BaseAdapter {
 		TextView price;
 		TextView title;
 		TextView callPhone;
-
+		
 		public ViewHolder(View itemView) {
 			super(itemView);
 			img = (DynamicHeightImageView) itemView.findViewById(R.id.img);
@@ -67,25 +68,22 @@ public class BrandDetailAdapter extends BaseAdapter {
 			callPhone = (TextView) itemView.findViewById(R.id.callPhone);
 		}
 	}
-
+	
 	@Override
 	public int getCount() {
 		// TODO Auto-generated method stub
 		return data == null ? 0 : data.length();
 	}
-
 	@Override
 	public Object getItem(int position) {
 		// TODO Auto-generated method stub
 		return data.opt(position);
 	}
-
 	@Override
 	public long getItemId(int position) {
 		// TODO Auto-generated method stub
 		return position;
 	}
-
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
 		ViewHolder viewHolder = null;
@@ -100,7 +98,6 @@ public class BrandDetailAdapter extends BaseAdapter {
 		viewHolder.title.setText(Tools.optString(item, "Productname", ""));
 		viewHolder.title.setTextSize(15);
 		viewHolder.content.setText(Tools.optString(item, "SubTitle", ""));
-
 		viewHolder.price.setText("均价" + Tools.optDouble(item, "Price", 0) + "元/m²起");
 		viewHolder.callPhone.setTag(Tools.optString(item, "Phone", ""));
 		viewHolder.callPhone.setOnClickListener(new OnClickListener() {
@@ -123,7 +120,6 @@ public class BrandDetailAdapter extends BaseAdapter {
 					MyApplication.getLoadingListener());
 		}
 		convertView.setOnClickListener(new OnClickListener() {
-
 			@Override
 			public void onClick(View v) {
 				ProductDetailActivity_.intent(mContext).productId(item.optString("Id")).start();
