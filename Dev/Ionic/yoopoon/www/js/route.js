848373cf3c30a8d13e99462dc598c8b22a6c79c3
gaofengming <1013882794@qq.com>
2015-09-09 18:04:26 +0800

    。。。

--- Dev/Ionic/yoopoon/www/js/route.js
+++ Dev/Ionic/yoopoon/www/js/route.js
@@ -117,7 +117,15 @@ app.config(function($stateProvider, $urlRouterProvider) {
                controller: 'TabMeCtrl'
                }
                }
-               });
+               })
+    .state('page.Product-Catagory', {
+        url: '/shopping/Product-Catagory',
+        views: {
+            'page-shopping': {
+                templateUrl: 'page/shopping/Product-Catagory.html'
+            }
+        }
+    });
         
         // if none of the above states are matched, use this as the fallback
         $urlRouterProvider.otherwise('/page/shopping');
