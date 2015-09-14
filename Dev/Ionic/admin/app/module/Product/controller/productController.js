2f6fcdab4a98e0c5fcd86a630d52f60100ec6547
yangyue <624866727@qq.com>
2015-09-14 09:37:45 +0800

    提交图片上传补充

--- Dev/Ionic/admin/app/module/Product/controller/productController.js
+++ Dev/Ionic/admin/app/module/Product/controller/productController.js
@@ -76,6 +76,18 @@ app.controller('editProductCtr',['$http','$scope','$state','$stateParams',functi
         {
             $scope.product.Img1=Img1;
         }
+        if(Img2.length>0)
+        {
+            $scope.product.Img2=Img2;
+        }
+        if(Img3.length>0)
+        {
+            $scope.product.Img3=Img3;
+        }
+        if(Img4.length>0)
+        {
+            $scope.product.Img4=Img4;
+        }
         $http.put(SETTING.ZergWcApiUrl+'/CommunityProduct/Put',$scope.product,{
             'withCredentials':true
         }).success(function(data){
@@ -166,7 +178,7 @@ function updatemainImage(file) {
     var div = document.getElementById('main');
     files = file.files[0];
     if (file.files && files) {
-        div.innerHTML = '<img style="height: 200px;width: 200px" id=mainimg>';
+        div.innerHTML = '<img style="height: 150px;width: 150px" id=mainimg>';
         var img = document.getElementById('mainimg');
         var reader = new FileReader();
         reader.onload = function (evt) {
@@ -212,7 +224,7 @@ function updatevcImg(file) {
     var div = document.getElementById('vc');
     files = file.files[0];
     if (file.files && files) {
-        div.innerHTML = '<img style="height: 200px;width: 200px" id=vcimg>';
+        div.innerHTML = '<img style="height: 150px;width: 150px" id=vcimg>';
         var img = document.getElementById('vcimg');
         var reader = new FileReader();
         reader.onload = function (evt) {
@@ -239,11 +251,11 @@ function updatevcImg(file) {
             //回调函数
             function callback () {
                 //将response提取出来分割出文件名
-                Img1=  xmlhttp.response;
-                var g1=Img1.split(':"');
-                var g2= Img1.split(',')[1].split(':"')[1];
+                Img=  xmlhttp.response;
+                var g1=Img.split(':"');
+                var g2= Img.split(',')[1].split(':"')[1];
                 //将分割好的文件名赋予给img全局变量
-                Img1=g2.substring(0,g2.length-1);
+                Img=g2.substring(0,g2.length-1);
                 //图片上传成功字样样式
                 headtext.innerHTML = '上传成功!';
                 headtext.style.color ='red';
@@ -257,7 +269,7 @@ function updatevcImg1(file) {
     var div = document.getElementById('vc1');
     files = file.files[0];
     if (file.files && files) {
-        div.innerHTML = '<img style="height: 200px;width: 200px" id=vcimg1>';
+        div.innerHTML = '<img style="height: 150px;width: 150px" id=vcimg1>';
         var img = document.getElementById('vcimg1');
         var reader = new FileReader();
         reader.onload = function (evt) {
@@ -285,10 +297,144 @@ function updatevcImg1(file) {
             function callback () {
                 //将response提取出来分割出文件名
                 Img1=  xmlhttp.response;
-                var g1=Img1.split(':"');
-                var g2= Img1.split(',')[1].split(':"')[1];
+                var g3=Img1.split(':"');
+                var g4= Img1.split(',')[1].split(':"')[1];
                 //将分割好的文件名赋予给img全局变量
-                Img1=g2.substring(0,g2.length-1);
+                Img1=g4.substring(0,g4.length-1);
+                //图片上传成功字样样式
+                headtext.innerHTML = '上传成功!';
+                headtext.style.color ='red';
+            }
+        }
+        reader.readAsDataURL(files);
+    }
+}
+
+
+function updatevcImg2(file) {
+    var div = document.getElementById('vc2');
+    files = file.files[0];
+    if (file.files && files) {
+        div.innerHTML = '<img style="height: 150px;width: 150px" id=vcimg2>';
+        var img = document.getElementById('vcimg2');
+        var reader = new FileReader();
+        reader.onload = function (evt) {
+            //base64编码
+            img.src = evt.target.result;
+            //扩展名
+            var ext = file.value.substring(file.value.lastIndexOf(".") + 1).toLowerCase();
+            // gif在ie浏览器不显示
+            if (ext != 'png' && ext != 'jpg' && ext != 'jpeg' && ext != 'gif') {
+                alert("只支持JPG,PNG,JPEG格式的图片");
+                return;
+            }
+            //发送请求
+            var xmlhttp=new XMLHttpRequest();
+            xmlhttp.onreadystatechange = callback;
+            var fd = new FormData();
+            xmlhttp.open("POST",SETTING.ZergWcApiUrl+'/Resource/Upload');
+            fd.append("fileToUpload",files);
+            xmlhttp.withCredentials = true;
+            xmlhttp.send(fd);
+            var headtext = document.getElementById("vc1text2");
+            headtext.innerHTML = '正在上传..';
+            headtext.style.color ='#40AD32'
+            //回调函数
+            function callback () {
+                //将response提取出来分割出文件名
+                Img2=  xmlhttp.response;
+                var g1=Img2.split(':"');
+                var g2= Img2.split(',')[1].split(':"')[1];
+                //将分割好的文件名赋予给img全局变量
+                Img2=g2.substring(0,g2.length-1);
+                //图片上传成功字样样式
+                headtext.innerHTML = '上传成功!';
+                headtext.style.color ='red';
+            }
+        }
+        reader.readAsDataURL(files);
+    }
+}
+function updatevcImg3(file) {
+    var div = document.getElementById('vc3');
+    files = file.files[0];
+    if (file.files && files) {
+        div.innerHTML = '<img style="height: 150px;width: 150px" id=vcimg3>';
+        var img = document.getElementById('vcimg3');
+        var reader = new FileReader();
+        reader.onload = function (evt) {
+            //base64编码
+            img.src = evt.target.result;
+            //扩展名
+            var ext = file.value.substring(file.value.lastIndexOf(".") + 1).toLowerCase();
+            // gif在ie浏览器不显示
+            if (ext != 'png' && ext != 'jpg' && ext != 'jpeg' && ext != 'gif') {
+                alert("只支持JPG,PNG,JPEG格式的图片");
+                return;
+            }
+            //发送请求
+            var xmlhttp=new XMLHttpRequest();
+            xmlhttp.onreadystatechange = callback;
+            var fd = new FormData();
+            xmlhttp.open("POST",SETTING.ZergWcApiUrl+'/Resource/Upload');
+            fd.append("fileToUpload",files);
+            xmlhttp.withCredentials = true;
+            xmlhttp.send(fd);
+            var headtext = document.getElementById("vc1text3");
+            headtext.innerHTML = '正在上传..';
+            headtext.style.color ='#40AD32'
+            //回调函数
+            function callback () {
+                //将response提取出来分割出文件名
+                Img3=  xmlhttp.response;
+                var g1=Img3.split(':"');
+                var g2= Img3.split(',')[1].split(':"')[1];
+                //将分割好的文件名赋予给img全局变量
+                Img3=g2.substring(0,g2.length-1);
+                //图片上传成功字样样式
+                headtext.innerHTML = '上传成功!';
+                headtext.style.color ='red';
+            }
+        }
+        reader.readAsDataURL(files);
+    }
+}
+function updatevcImg4(file) {
+    var div = document.getElementById('vc4');
+    files = file.files[0];
+    if (file.files && files) {
+        div.innerHTML = '<img style="height: 150px;width: 150px" id=vcimg4>';
+        var img = document.getElementById('vcimg4');
+        var reader = new FileReader();
+        reader.onload = function (evt) {
+            //base64编码
+            img.src = evt.target.result;
+            //扩展名
+            var ext = file.value.substring(file.value.lastIndexOf(".") + 1).toLowerCase();
+            // gif在ie浏览器不显示
+            if (ext != 'png' && ext != 'jpg' && ext != 'jpeg' && ext != 'gif') {
+                alert("只支持JPG,PNG,JPEG格式的图片");
+                return;
+            }
+            //发送请求
+            var xmlhttp=new XMLHttpRequest();
+            xmlhttp.onreadystatechange = callback;
+            var fd = new FormData();
+            xmlhttp.open("POST",SETTING.ZergWcApiUrl+'/Resource/Upload');
+            fd.append("fileToUpload",files);
+            xmlhttp.withCredentials = true;
+            xmlhttp.send(fd);
+            var headtext = document.getElementById("vc1text4");
+            headtext.innerHTML = '正在上传..';
+            headtext.style.color ='#40AD32'
+            //回调函数
+            function callback () {
+                //将response提取出来分割出文件名
+                Img4=  xmlhttp.response;
+                var g1=Img4.split(':"');
+                var g2= Img4.split(',')[1].split(':"')[1];
+                //将分割好的文件名赋予给img全局变量
+                Img4=g2.substring(0,g2.length-1);
                 //图片上传成功字样样式
                 headtext.innerHTML = '上传成功!';
                 headtext.style.color ='red';
