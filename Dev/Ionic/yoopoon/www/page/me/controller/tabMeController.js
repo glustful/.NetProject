8f2d69a104457a585a9108e8f8860098af434cb4
chen <944845129@qq.com>
2015-09-15 15:07:37 +0800

    根据UserId获取会员信息

--- Dev/Ionic/yoopoon/www/page/me/controller/tabMeController.js
+++ Dev/Ionic/yoopoon/www/page/me/controller/tabMeController.js
@@ -1,4 +1,4 @@
-app.controller('TabMeCtrl', function($scope, $ionicSlideBoxDelegate) {
+app.controller('TabMeCtrl','$http', function($scope,$http, $ionicSlideBoxDelegate) {
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
   // To listen for when this page is active (for example, to refresh data),
@@ -23,13 +23,22 @@ app.controller('TabMeCtrl', function($scope, $ionicSlideBoxDelegate) {
   $scope.delegateHandler = $ionicSlideBoxDelegate;
 
 
-
-
 //打开评论
   var comment=document.getElementById("userComment");
   $scope.open=function(){
     comment.style.display="";
   }
 
-               });
+  //获取当前会员信息
+  var getMemById = function(){
+    $http.get(SETTING.ZergWcApiUrl+"/Member/Get?id="+$stateParams.id,{
+      'withCredentials':true
+    }).success(function(data){
+      $scope.memModels=data;
+    });
+  }
+  getMemById();
+
+
+});
 
