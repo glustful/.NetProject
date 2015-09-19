bf2a15d046ff0e6e911fbd3fc57ecaacd8d5a1fb
guojunjun <852411097@qq.com>
2015-09-19 16:26:39 +0800

    创建启动画

--- Dev/Ionic/yoopoon/www/js/route.js
+++ Dev/Ionic/yoopoon/www/js/route.js
@@ -49,7 +49,7 @@ app.config(function($stateProvider, $urlRouterProvider) {
         // Set up the various states which the app can be in.
         // Each state's controller can be found in controllers.js
         $stateProvider
-        
+
         // setup an abstract state for the tabs directive
         .state('page', {
                url: '/page',
