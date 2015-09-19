52e68544023983def53f7d6ef98df9f81f99c572
pengjiayuan <286617690@qq.com>
2015-09-19 09:59:56 +0800

    LazyViewPager

---
+++ Dev/Android/YoopoonMarket/src/com/yoopoon/market/LoginActivity.java
@@ -0,0 +1,43 @@
+package com.yoopoon.market;
+
+import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.EActivity;
+import android.content.Intent;
+import android.view.View;
+
+@EActivity(R.layout.activity_login)
+public class LoginActivity extends MainActionBarActivity {
+
+	@AfterViews
+	void initUI() {
+		backButton.setVisibility(View.VISIBLE);
+		titleButton.setVisibility(View.VISIBLE);
+		rightButton.setVisibility(View.GONE);
+		backWhiteButton.setVisibility(View.GONE);
+		titleButton.setText("账户登录");
+	}
+
+	@Override
+	public void backButtonClick(View v) {
+		// 点击返回，一定是没有登陆成功，返回首页
+		Intent intent = new Intent("com.yoopoon.market.shopfragment");
+	}
+
+	@Override
+	public void titleButtonClick(View v) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void rightButtonClick(View v) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Boolean showHeadView() {
+		return true;
+	}
+
+}
