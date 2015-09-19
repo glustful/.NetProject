157130d285d668ddacab5d484ad465606cd37a25
彭贵飞 <944015456@qq.com>
2015-09-19 16:29:21 +0800

    添加属性

--- Dev/Ionic/yoopoon/www/page/shopping/controller/CategoryController.js
+++ Dev/Ionic/yoopoon/www/page/shopping/controller/CategoryController.js
@@ -2,7 +2,7 @@
  * Created by huangxiuyu on 2015/9/15.
  */
 //start----------------------------商品分类 huangxiuyu2015.09.15-------------------------
-app.controller('CategoryController',['$scope','$http',function($scope,$http){
+app.controller('CategoryController',['$scope','$http','$state',function($scope,$http,$state){
     $scope.searchCondition={
         ifid:0
     }
@@ -16,6 +16,9 @@ app.controller('CategoryController',['$scope','$http',function($scope,$http){
             })
     };
     $scope.selectCategory(1);
-
+    $scope.productName = '';
+    document.getElementById('search').onblur = function () {
+        $state.go("page.search_product", {productName: $scope.productName});
+    };
 }]);
 //end----------------------------商品分类 huangxiuyu2015.09.15-------------------------
