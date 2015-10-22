59fc53b3ce4dc4d9bd0785bad3dfa5d74f1f23d5
gaofengming <1013882794@qq.com>
2015-10-22 17:16:51 +0800

    分类页修改

--- Dev/Ionic/yoopoon/www/page/shopping/controller/CategoryController.js
+++ Dev/Ionic/yoopoon/www/page/shopping/controller/CategoryController.js
@@ -3,23 +3,30 @@
  */
 //start----------------------------商品分类 huangxiuyu2015.09.15-------------------------
 app.controller('CategoryController',['$scope','$http','$state',function($scope,$http,$state){
-    $scope.searchCondition={
-        ifid:0
+    //$scope.searchCondition={
+    //    ifid:0
+    //}
+    //$scope.selectCategory=function(ifid){
+    //    //alert("fds");
+    //    $scope.searchCondition.ifid=ifid;
+    //    $http.get(SETTING.ApiUrl+'/Category/GetAllTree/',{params:$scope.searchCondition,'withCredentials':
+    //        true}).
+    //        success(function(data){
+    //            $scope.list=data;
+    //            console.log(data);
+    //        })
+    //};
+    //$scope.selectCategory(1);
+    //$scope.productName = '';
+    //document.getElementById('search').onblur = function () {
+    //    $state.go("page.search_product", {productName: $scope.productName});
+    //};
+    $scope.tabIndex= 1 ;
+    $scope.category=function(tabIndex,$event){
+        $scope.obj=$event.target;
+        $scope.tabIndex=tabIndex;
+
+
     }
-    $scope.selectCategory=function(ifid){
-        //alert("fds");
-        $scope.searchCondition.ifid=ifid;
-        $http.get(SETTING.ApiUrl+'/Category/GetAllTree/',{params:$scope.searchCondition,'withCredentials':
-            true}).
-            success(function(data){
-                $scope.list=data;
-                console.log(data);
-            })
-    };
-    $scope.selectCategory(1);
-    $scope.productName = '';
-    document.getElementById('search').onblur = function () {
-        $state.go("page.search_product", {productName: $scope.productName});
-    };
 }]);
 //end----------------------------商品分类 huangxiuyu2015.09.15-------------------------
